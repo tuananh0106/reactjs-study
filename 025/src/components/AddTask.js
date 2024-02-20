0eecc7949d2f90ffebba342d21d1b053a29bdb30
tuananh0106 <90133342+tuananh0106@users.noreply.github.com>
2024-02-20 15:21:19 +0700

    update create new task
    create new task

---
+++ 025/src/components/AddTask.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./AddTask.css";
+
+export const AddTask = () => {
+  const [taskValue, setTaskValue] = useState();
+  const [progress, setProgress] = useState(false);
+
+  const handleChange = (event) => {
+    setTaskValue(event.target.value);
+  };
+
+  const handleReset = () => {
+    setTaskValue("");
+    setProgress(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const task = {
+      id: Math.floor(Math.random() * 10000),
+      name: taskValue,
+      complete: Boolean(progress),
+    };
+    console.log(task);
+    handleReset();
+  };
+
+  return (
+    <section className="addtask">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="task"> Task Name :</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          id="task"
+          name="task"
+          placeholder="Task"
+          autoComplete="off"
+          value={taskValue}
+        ></input>
+        <select onChange={(e) => setProgress(e.target.value)}>
+          <option value="false">Pendding</option>
+          <option value="true">Completed</option>
+        </select>
+        <button type="submit">Add Task</button>
+        <span onClick={handleReset} className="reset">
+          Reset
+        </span>
+        <p>{taskValue}</p>
+      </form>
+    </section>
+  );
+};
