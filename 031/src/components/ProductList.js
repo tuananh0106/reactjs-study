29a60379947e47cffd49526e46a339c2a6a251f4
tuananh0106 <90133342+tuananh0106@users.noreply.github.com>
2024-02-21 15:15:25 +0700

    add loading
    add loading

--- 031/src/components/ProductList.js
+++ 031/src/components/ProductList.js
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
+import Loading from "../assets/loading.gif";
 
 export const ProductList = () => {
   const [url, setUrl] = useState("http://localhost:8000/products");
-  const { data: products } = useFetch(url);
+  const { data: products, loading } = useFetch(url);
 
   return (
     <section>
@@ -17,6 +18,12 @@ export const ProductList = () => {
           In Stock Only
         </button>
       </div>
+
+      {loading && (
+        <p className="loading">
+          <img src={Loading} alt="" />
+        </p>
+      )}
 
       {products &&
         products.map((product) => (
