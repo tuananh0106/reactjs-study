077f614fdc4c82b5759b54b2202cac6ab412b05d
tuananh0106 <90133342+tuananh0106@users.noreply.github.com>
2024-02-21 11:28:27 +0700

    add fitter
    add fitter

--- 031/src/components/ProductList.js
+++ 031/src/components/ProductList.js
@@ -2,17 +2,29 @@ import React, { useEffect, useRef, useState } from "react";
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState("http://localhost:8000/products");
 
   console.log(products);
 
   useEffect(() => {
-    fetch("http://localhost:8000/products")
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setProducts(data));
-  }, []);
+  }, [url]);
 
   return (
     <section>
+      <div className="filter">
+        <button onClick={() => setUrl("http://localhost:8000/products")}>
+          All
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}
+        >
+          In Stock Only
+        </button>
+      </div>
+
       {products.map((product) => (
         <div className="card" key={product.id}>
           <p className="id">{product.id}</p>
