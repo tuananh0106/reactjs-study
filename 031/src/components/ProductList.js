e682b020be90f505cf20217b0c2feb74df543d14
tuananh0106 <90133342+tuananh0106@users.noreply.github.com>
2024-02-21 11:39:17 +0700

    useCallBack how to use in reactJS
    useCallBack how to use in reactJS

--- 031/src/components/ProductList.js
+++ 031/src/components/ProductList.js
@@ -1,16 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [url, setUrl] = useState("http://localhost:8000/products");
 
-  console.log(products);
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
+  }, [url]);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, [url]);
+    fetchProducts();
+  }, [fetchProducts]);
 
   return (
     <section>
