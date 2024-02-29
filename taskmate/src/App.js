631be75c9a151bbc6cc099c3e07d534c38ad132f
tuananh0106 <90133342+tuananh0106@users.noreply.github.com>
2024-02-29 11:20:56 +0700

    add local storage to task list
    add local storage to task list

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AddTask } from "./components/AddTask";
 import { Header } from "./components/Header";
 import { ShowTask } from "./components/ShowTask";
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem("taskList")) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }, [taskList]);
+
   return (
     <div className="App">
       <Header />
